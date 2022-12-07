82c49f451c6fa15942c9b51f18c731c0efb83290
kana2022 <kkanat94@gmail.com>
2022-12-07 11:04:56 +0600

    test

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int[] numbers = new int[] { 10, 12, 13, 14, 15, 16, 17, 18 };
+Rank(3, numbers);
+static int Rank(int key, int[] numbers)
+{
+    int low = 0;
+    int high = numbers.Length - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (key < numbers[mid]) high = mid - 1;
+        else if (key > numbers[mid]) low = mid + 1;
+        else return mid;
+    }
+    return -1;
+}
